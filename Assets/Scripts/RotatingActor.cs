f5b1ed23d26c90e1e07aed28d86b58f3eb30f70f
Orilexx <teo.martin777@gmail.com>
2022-01-12 17:37:33 +0100

    Polished enemy projectile

---
+++ Assets/Scripts/RotatingActor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingActor : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 360.0f) * 2 * Time.deltaTime);
+    }
+}
